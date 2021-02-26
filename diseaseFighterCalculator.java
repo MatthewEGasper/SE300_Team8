3a4d3abf687f41d98699b07287e94ba7d4107efc
Elijah Jenkins <ej31199@outlook.com>
2021-02-25 20:54:41 -0500

    Created diseaseFighterCalculator Class

---
+++ diseaseFighterCalculator.java
@@ -0,0 +1,29 @@
+/* File: diseaseFighterCalculator.java
+ * Name: Disease Fighter Calculator
+ * Desc: This class is meant to simulate the disease's battle with the body's immune system
+ * Auth: Elijah Jenkins
+ * Date: 2/24/2021
+ */
+
+ Public class diseaseFighterCalculator {
+     Person person = new Person;
+     Disease disease = new Disease;
+
+    resistance = person.getImmuneSystemResistance();
+    health = person.getHealth();
+    lifeState = person.getLifeState();
+
+     lethality = disease.getLethality();
+     lifespan = disease.getLifespan();
+
+//disease fighter functionality -> Use combination of lethality and resistance (and random numbers) to 
+//either increase or decrease person's health. resistance could be lowered once person's health drops 
+//below certain threshold, say 15%
+
+if (lifeState != 0) {
+    //run the calculator
+    
+
+}
+
+ }
