4b0890e7cdea4633b93917016f25b4836fb499ba
Bryan Polk <brypolkgmail.com>
2021-02-27 06:20:48 -0500

    Added functionality and template classes
    Template classes to support basic DiseaseFighterCalculator functionality. Work in progress.

---
+++ Zombie Game/src/Disease.java
@@ -0,0 +1,24 @@
+//template class to be updated with Disease class.
+public class Disease {
+
+	public float getLethality() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getLifespan() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setLethality(float lethality) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLifespan(int lifespan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Zombie Game/src/Person.java
@@ -0,0 +1,44 @@
+//template class to be updated with Person class.
+public class Person {
+
+	public float getImmuneSystemResistance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getHealth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean getLifeState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Disease getDisease() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setImmuneSystemResistance(float resistance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHealth(int health) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setLifeState(boolean lifeState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDisease(Disease disease) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
